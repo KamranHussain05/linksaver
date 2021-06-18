21b5f25bf4ab7ba536a4ab3a3a425ca998001a64
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-06-18 12:09:46 -0700

    i think its done

--- src/Data.py
+++ src/Data.py
@@ -3,13 +3,24 @@ class Data:
     def __init__(self, num):
         for x in range num:
             strings.append("")
-    def replaceStrings(int num, String s):
+    def replaceStrings( num, s):
 		strings.pop(num)
 		strings.insert(num,s)
-    def getFullString(int n):
+    def getFullString(n):
         return strings[n]
-    def getCourseName(int n):
-        String s = strings[n]
+    def getCourseName(n):
+        r = ""
+        s = strings[n]
         if(s.index(";") != -1):
-            
+            newString = strings[s.index(';')+1:]
+            n = newString.find(";")
+            return strings[s.index(';'):n]
+        else:
+            return ""
+    def getMeetingLink(n):
+
+    def printStrings():
+        for s in strings:
+            print(s)
+        
 
