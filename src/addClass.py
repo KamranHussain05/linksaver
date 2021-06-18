e88766eac012a1acd39c5e9579ec63252e5ad6fc
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-06-18 10:15:42 -0700

    Added Add Class GUI and backend

---
+++ src/addClass.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+
+class AddClass(QDialog):
+    def __init__(self):
+        super(AddClass,self).__init__()
+        loadUi("addClassGUI.ui",self)
+        self.classEntered.clicked.connect(self.loginfunction)
+
+    def loginfunction(self):
+        courseName=self.className.text()
+        courseLink=self.classLink.text()
+        meetingLink=self.meetingLink.text()
+        print("All Class Data entered")
+        print("Class Name: " + courseName)
+        print("Class Link: " + courseLink)
+        print("Meeting Link: " + meetingLink)
+
+app=QApplication(sys.argv)
+mainwindow=AddClass()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(480)
+widget.setFixedHeight(620)
+widget.show()
+app.exec_()
