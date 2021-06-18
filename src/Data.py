241cc695e8564ab971f525c4f126042f577adf12
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-06-18 11:07:46 -0700

    need to figure out substrings

--- src/Data.py
+++ src/Data.py
@@ -3,3 +3,13 @@ class Data:
     def __init__(self, num):
         for x in range num:
             strings.append("")
+    def replaceStrings(int num, String s):
+		strings.pop(num)
+		strings.insert(num,s)
+    def getFullString(int n):
+        return strings[n]
+    def getCourseName(int n):
+        String s = strings[n]
+        if(s.index(";") != -1):
+            
+
