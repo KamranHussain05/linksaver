c86893ca825f2fe42d620827309817e0273d29c0
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-06-18 10:45:01 -0700

    Fixed Add Class Sizing error

--- .qt_for_python/uic/addClassGUI.py
+++ .qt_for_python/uic/addClassGUI.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(710, 521)
+        Dialog.resize(710, 512)
         Dialog.setStyleSheet("background-color: rgb(68, 68, 68);\n"
 "font-size: 14pt;")
         self.label = QtWidgets.QLabel(Dialog)
--- .qt_for_python/uic/home.py
+++ .qt_for_python/uic/home.py
@@ -11,60 +11,28 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(710, 521)
-        Dialog.setStyleSheet("background-color: rgb(68, 68, 68);\n"
-"font-size: 14pt;")
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(250, 40, 201, 61))
-        self.label.setMouseTracking(False)
-        self.label.setStyleSheet("color: rgb(253, 253, 255);\n"
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1406, 814)
+        MainWindow.setStyleSheet("background-color:   rgb(68, 68, 68);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 1411, 101))
+        self.label.setStyleSheet("background-color: rgb(86, 124, 158);\n"
+"color: rgb(255,255,255);\n"
 "font-size:28pt;\n"
-"text-align: center;")
+"text-align: center;\n"
+"")
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(20, 160, 121, 41))
-        self.label_2.setStyleSheet("color: rgb(255, 232, 162);\n"
-"font-size: 16pt;")
-        self.label_2.setObjectName("label_2")
-        self.className = QtWidgets.QLineEdit(Dialog)
-        self.className.setGeometry(QtCore.QRect(170, 150, 531, 61))
-        self.className.setStyleSheet("color: rgb(255, 255, 255)")
-        self.className.setObjectName("className")
-        self.classEntered = QtWidgets.QPushButton(Dialog)
-        self.classEntered.setGeometry(QtCore.QRect(0, 460, 711, 61))
-        self.classEntered.setStyleSheet("background-color: rgb(206, 197, 146)")
-        self.classEntered.setObjectName("classEntered")
-        self.className_2 = QtWidgets.QLineEdit(Dialog)
-        self.className_2.setGeometry(QtCore.QRect(170, 240, 531, 61))
-        self.className_2.setStyleSheet("color: rgb(255, 255, 255)")
-        self.className_2.setObjectName("className_2")
-        self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(20, 250, 111, 41))
-        self.label_3.setStyleSheet("color: rgb(255, 232, 162);\n"
-"font-size: 16pt;")
-        self.label_3.setObjectName("label_3")
-        self.className_3 = QtWidgets.QLineEdit(Dialog)
-        self.className_3.setGeometry(QtCore.QRect(170, 330, 531, 61))
-        self.className_3.setStyleSheet("color: rgb(255, 255, 255)")
-        self.className_3.setObjectName("className_3")
-        self.label_4 = QtWidgets.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(20, 340, 121, 41))
-        self.label_4.setStyleSheet("color: rgb(255, 232, 162);\n"
-"font-size: 16pt;")
-        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(_translate("Dialog", "Add a Class"))
-        self.label_2.setText(_translate("Dialog", "Class Name"))
-        self.classEntered.setText(_translate("Dialog", "Enter"))
-        self.label_3.setText(_translate("Dialog", "CourseLink"))
-        self.label_4.setText(_translate("Dialog", "Meeting Link"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Link-Saver"))
