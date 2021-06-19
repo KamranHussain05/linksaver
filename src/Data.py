c9701e7c2cea70ebe0d9ccf1ad641edc3bc9a57c
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-06-19 09:33:15 -0700

    new function should work

--- src/Data.py
+++ src/Data.py
@@ -31,35 +31,34 @@ class Data:
             print("not a valid index!")
             return
         s = self.strings[n]
-        if (s.index(";") != -1):
+        if (s.find(";") != -1):
             newString = self.strings[s.index(';') + 1:]
             endInd = newString.find(";")
             return self.strings[s.index(';'):endInd]
         else:
             return ""
 
-    # def getCourseLink(self, n):
-    #     if (n>self.total):
-    #         print("not a valid index!")
-    #         return
-    #     s = self.strings[n]
-    #     if (s.index(";") != -1 and s.rindex(";") != -1):
-    #
-    #         # s.substring(s.indexOf(';')+1, s.lastIndexOf(';')); => ?
-    #         # s.substring(s.lastIndexOf(';')+1); => self.strings[s.index(';') + 1:]
-    #
-    #         newString = self.strings[s.index(';') + 1:]
-    #         endInd = newString.find(";")
-    #         return self.strings[endInd]
-    #     else:
-    #         return ""
+    def getCourseLink(self, n):
+        if (n>self.total):
+            print("not a valid index!")
+            return
+        s = self.strings[n]
+        r = s[s.find(";")+1:]
+        rindex = r.find(";")
+        if (s.find(";") != -1 and rindex != -1):
+            s[s.find(';')+1: rindex]
+            self.strings[s.index(';') + 1:]
+            newString = self.strings[s.index(';') + 1:rindex]
+            return newString
+        else:
+             return ""
 
     def getMeetingLink(self, n):
         if n > self.total:
             print("not a valid index!")
             return
         s = self.strings[n]
-        if s.index(";") != -1:
+        if s.find(";") != -1:
             # error here - code disappears
             newString = self.strings[s.index(';') + 1:]
             endInd = newString.find(";")
