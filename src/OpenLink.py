20c3095366fdbaca22666ec92679b2fe23af4c8d
alexwang05 <hpwzz20@gmail.com>
2021-06-18 22:41:46 -0700

    add openLInk()

--- src/OpenLink.py
+++ src/OpenLink.py
@@ -1,21 +1,27 @@
 import webbrowser, validators
 
+
 class LinkOpener:
     theURL = ""
-    def __init__ (self, toOpen):
+
+    def __init__(self, toOpen):
         theURL = toOpen
         if (self.isValid(theURL)):
             webbrowser.open(theURL)
-    
+
     def isValid(self, urlStr):
-        
+
         if (urlStr[0] != 'h' or urlStr[1] != 't'):
             urlStr = "https://" + urlStr
             self.theURL = urlStr
-        valid=validators.url(urlStr)   
+        valid = validators.url(urlStr)
         if (valid):
             return True
         else:
             print("this link is not valid!")
             return False
 
+    # pass in string to open
+    @staticmethod
+    def openLink(s):
+        webbrowser.open(s)
