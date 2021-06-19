bcebd5969632a332994e8d07f6ab78e175e0fdb1
alexwang05 <hpwzz20@gmail.com>
2021-06-18 17:34:34 -0700

    format code

--- src/addClass.py
+++ src/addClass.py
@@ -1,29 +1,24 @@
-#Author: Kamran Hussain
-#Date: 6/18/21
-#Dependencies: PyQt5, home.ui, homegui.py, qt -> addClassGui.py
+# Author: Kamran Hussain
+# Date: 6/18/21
+# Dependencies: PyQt5, home.ui, homegui.py, qt -> addClassGui.py
 from FileChanger import FileChanger
-from HomeGui import HomeGui
+import HomeGui
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 
-#adds class
+
+# adds class
 class AddClass(QDialog):
     def __init__(self):
-        super(AddClass,self).__init__()
-        loadUi("addClassGUI.ui",self)
+        super(AddClass, self).__init__()
+        loadUi("addClassGUI.ui", self)
         self.classEntered.clicked.connect(self.classDataInput)
 
     # called when user presses save
     def classDataInput(self):
-        courseName=self.className.text()
-        courseLink=self.classLink.text()
-        meetingLink=self.meetingLink.text()
-        print("All Class Data entered")
-        print("Class Name: " + courseName)
-        print("Class Link: " + courseLink)
-        print("Meeting Link: " + meetingLink)
+        AddClass.writeToList()
         AddClass.close()
 
     # Write to list
@@ -44,9 +39,9 @@ class AddClass(QDialog):
         HomeGui.refresh();
 
 
-app=QApplication(sys.argv)
-mainwindow=AddClass()
-widget=QtWidgets.QStackedWidget()
+app = QApplication(sys.argv)
+mainwindow = AddClass()
+widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
 widget.setFixedWidth(710)
 widget.setFixedHeight(512)
