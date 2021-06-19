10958fe1f7d236ef089ae019b03c61e932280f92
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-06-18 20:53:19 -0700

    Updated new Home Gui

--- src/HomeGui.py
+++ src/HomeGui.py
@@ -1,3 +1,23 @@
 # Author: Kamran Hussain
 # Date: 4/18/21
 # Dependencies
+
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+
+
+class Home(QDialog):
+    def __init__(self):
+        super(Home, self).__init__()
+        loadUi("homeGui.ui", self)
+
+
+app = QApplication(sys.argv)
+mainwindow = Home()
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+
+widget.show()
+app.exec_()
