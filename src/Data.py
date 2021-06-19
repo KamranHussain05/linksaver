29cfac72bc227a4e3b5f6548160d8df1ab727756
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-06-19 09:51:03 -0700

    new commit

--- src/Data.py
+++ src/Data.py
@@ -1,4 +1,3 @@
-from FileChanger import FileChanger
 
 class Data:
     strings = []
@@ -6,6 +5,7 @@ class Data:
 
     def __init__(self, num):
         total = num
+
         for x in range(num):
             self.strings.append("")
 
@@ -15,8 +15,6 @@ class Data:
             return
         self.strings.pop(pos)
         self.strings.insert(pos, val)
-
-    def returnStrings(self):
         return self.strings
 
     def getFullString(self, n):
@@ -32,31 +30,17 @@ class Data:
         s = self.strings[n]
         if (s.find(";") != -1):
             newString = self.strings[s.index(';') + 1:]
-            endInd = newString.find(";")
-            return self.strings[s.index(';'):endInd]
-        else:
             return ""
 
     def getCourseLink(self, n):
         if (n>self.total):
             print("not a valid index!")
             return
-<<<<<<< HEAD
-        s = self.strings[n] 
-        
-=======
         s = self.strings[n]
         r = s[s.find(";")+1:]
         rindex = r.find(";")
-        if (s.find(";") != -1 and rindex != -1):
-            s[s.find(';')+1: rindex]
-            self.strings[s.index(';') + 1:]
-            newString = self.strings[s.index(';') + 1:rindex]
-            return newString
-        else:
-             return ""
+        return ""
 
->>>>>>> parent of 19f9f2c (remove check)
     def getMeetingLink(self, n):
         if n > self.total:
             print("not a valid index!")
