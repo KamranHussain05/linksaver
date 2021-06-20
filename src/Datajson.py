10e683a80e424653e462410c6f2161183f8da97d
LeoSivagar <79562011+LeoSivagar@users.noreply.github.com>
2021-06-19 20:02:14 -0700

    Removed unused files and added methods to Datajson.py

--- src/Datajson.py
+++ src/Datajson.py
@@ -6,9 +6,8 @@ class Datajson:
     def __init__(self):
         self.filePath = "Data.json"
         self.properties = properties = []
-        with open(self.filePath, 'r+') as propertiesJSON:
-            properties = json.load(propertiesJSON)
-
+        with open(self.filePath, 'r+') as self.propertiesJSON:
+            properties = json.load(self.propertiesJSON)
 
 
     # Used to the check if there is a 'data.txt' file
@@ -21,16 +20,30 @@ class Datajson:
         with open(self.filePath, 'w') as f:
             json.dump(dumpFile, f)
 
+
+    def editButton(self, buttonName, className, courselink, meetingLink):
+
+        for course in self.properties:
+            if(course['button'] == buttonName):
+                course['class_name'] = className
+                course['class_link'] = courselink
+                course['meeting_link'] = meetingLink
+            else:
+                self.addButtonFunction(self,buttonName, className, courselink, meetingLink)
+
     def addButtonFunction(self, buttonName, className, courselink, meetingLink):
 
         newClass = {
-            "Button": f"{buttonName}",
-            "Class_Name": f"{className}",
-            "Class_Link": f"{courselink}",
-            "Meeting_Link": f"{meetingLink}"
+            "button": f"{buttonName}",
+            "class_name": f"{className}",
+            "class_link": f"{courselink}",
+            "meeting_link": f"{meetingLink}"
         }
 
         if not self.check_file("data.json"):
             self.createDataFile(newClass)
         else:
-
+            self.propertiesJSON.append(newClass)
+            self.propertiesJSON.seek(0, 0)
+            js_format = json.dumps(self.properties, indent=4)
+            self.propertiesJSON.write(js_format)
