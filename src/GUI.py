c125d9dce205aab6ba7ec18052807909ed9a00d5
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-06-19 20:50:51 -0700

    Update GUI.py

--- src/GUI.py
+++ src/GUI.py
@@ -170,7 +170,6 @@ class Home(QDialog):
         # reset.launchCourse1.setText(QCoreApplication.translate("Dialog", u"test", None))
         print('Refreshed Home')
 
-
 # --------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------
 
